d72540776c565c496f1034174c43672c8190d729
Hoàng Tấn <itk160454@gmail.com>
2019-06-03 23:00:24 +0700

    commit finish app

---
+++ app/src/main/java/nhom1/project3/socialnetwork/Adapter/TabsPagerAdapter.java
@@ -0,0 +1,51 @@
+package nhom1.project3.socialnetwork.Adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import nhom1.project3.socialnetwork.Fragments.ChatsFragment;
+import nhom1.project3.socialnetwork.Fragments.RequestsFragment;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter{
+
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 1:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2; // 2 is total fragment number (e.x- Chats, Requests)
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "CHATS"; // ChatsFragment
+            case 1:
+                return "REQUESTS"; // ttttRequestsFragment
+            default:
+                return null;
+        }
+        //return super.getPageTitle(position);
+    }
+}
